dfa41bd9ab4424a0209a9e757c37100544300336
liambrady717 <bradyliam717@gmail.com>
2025-09-30 16:37:34 +0000

    ex4 to ex 6

---
+++ Exercise4.java
@@ -0,0 +1,8 @@
+public class Exercise4 {
+    public static void main(String[] args) {
+        System.out.println("The capital of ireland is:");
+        System.out.println("Dublin");
+        System.out.println("The patron saint of Ireland is");
+        System.out.println("St.Patrick");
+    }
+}
---
+++ Exercise5.java
@@ -0,0 +1,8 @@
+public class Exercise5 {
+    public static void main(String[] args) {
+        System.out.println("A     B");
+        System.out.println("=======");
+        System.out.println("1     2");
+    }
+}
+
